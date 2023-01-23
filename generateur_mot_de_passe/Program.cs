f4c1804bfab1bd6d338f42074572a3e125e0df2c
joel <joeldowono1@gmail.com>
2023-01-23 12:02:06 +0100

    implémentation de la classe outils

---
+++ generateur_mot_de_passe/Program.cs
@@ -0,0 +1,13 @@
+﻿using FormationCS;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int longeurMotDePasse = outils.DemanderNombrePositifNonNul("Longueur du mot de passe: ");
+        }
+    }
+}
---
+++ generateur_mot_de_passe/outils.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationCS
+{
+    static class outils
+    {
+        public static int DemanderNombrePositifNonNul(string question)
+        {
+            return DemanderNombreEntre(question, 1, int.MaxValue);
+        }
+
+        public static int DemanderNombreEntre(string question, int min, int max)
+        {
+            int nombre = DemanderNombre(question);
+            if ((nombre >= min) && (nombre <= max))
+            {
+                //valide
+                return nombre;
+            }
+            Console.WriteLine("ERREUR : le nombre doit être compris entre " + min + " et " + max);
+
+            return DemanderNombreEntre(question, min, max);
+        }
+
+        public static int DemanderNombre(string question)
+        {
+            while (true)
+            {
+                Console.Write(question);
+                string reponse = Console.ReadLine();
+
+                try
+                {
+                    int reponseInt = int.Parse(reponse);
+                    return reponseInt;
+                }
+                catch
+                {
+                    Console.WriteLine("Erreur: Vous devez rentrer un nombre");
+                }
+            }
+        }
+    }
+}
